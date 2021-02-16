8233469d34ecc677ddcec397fdd7f77f4e9b8ad3
meriambs <bensalah.meriem0@gmail.com>
2021-02-16 23:09:01 +0100

    fini partie get job offer

--- Partie Client/src/views/GetJobOffer/GetJobView/index.js
+++ Partie Client/src/views/GetJobOffer/GetJobView/index.js
@@ -1,15 +1,14 @@
-import React ,  { useState,useEffect } from 'react';
-import {
-  Container,
-  Grid,
-  makeStyles
-} from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import ButtonBase from '@material-ui/core/ButtonBase';
 import Page from 'src/components/Page';
 //  import GetDetails from './GetDetails';
  import axios from 'axios';
  import index from '../../JobOffer/ProductListView/ProductCard'
 import {useParams} from 'react-router-dom'
-
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,6 +16,9 @@ const useStyles = makeStyles((theme) => ({
     minHeight: '100%',
     paddingBottom: theme.spacing(3),
     paddingTop: theme.spacing(3)
+  },
+  productCard: {
+    height: '100%'
   }
 }));
 
@@ -24,61 +26,25 @@ const GetJobOffer = () => {
   const classes = useStyles();
  const {id}=useParams();
   console.log('id',id)
-//  const [products,setProducts] = useState([]);
-//   useEffect(() => {
-//      const fetchGetOff = async () =>{
-//        const res = await axios.get(`http://localhost:3003/product/${id}`)
-//        setProducts(res.data);
-//        console.log('res','data')
-//        // setVideo(res.data.video);
-//      }
-//      fetchGetOff();
-//    }, [])
-  return (
-    <Page
-      className={classes.root}
-      title="Account"
-    >
-      <Container maxWidth="lg">
-
-   {/* {data.filter(cart =>cart.id === id).map((cart, index)=>{
-     <div key={index}> */}
+  const [offer,setOffer] = useState();
+    useEffect(() => {
+      const fetchGetOff = async () =>{
+        const res = await axios.get(`http://localhost:3003/post/${id}`)
+        setOffer(res.data[0]);
+        console.log('res','data')
       
-     {/* </div>
-   })} */}
-        {/* <Grid
-          container
-          spacing={3}
-          
-        >
-          <Grid
-            item
-            lg={3}
-            md={3}
-            xs={12}
-          >
-          </Grid>
-          <Grid
-            item
-            lg={6}
-            md={6}
-            xs={12}
-          >
-         
-             {/* <GetDetails />  */}
-          {/* </Grid>
-                    <Grid
-            item
-            lg={3}
-            md={3}
-            xs={12}
-          >
-          </Grid> */}
-{/* 
-        </Grid> */} 
-      </Container>
-    </Page>
+       }
+       fetchGetOff();
+     }, [])
+  return (
+    <div className={classes.root}>
+      <h1>hoo</h1>
+      {offer.adress}
+      {offer.JobRequirements}
+    </div>
+    
   );
-};
+}
+
 
 export default GetJobOffer;
